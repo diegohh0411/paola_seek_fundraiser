56dbc5a96793e1915986f6356abf0cf5f857cf53
Diego Hernández Herrera <diegohh0411@gmail.com>
2024-11-08 16:22:01 -0600

    Ya está listo lo básico para que me donen

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Diego Hernandez | SEEK 25';
+export const SITE_DESCRIPTION = 'Un universitario católico de 20 años que necesita tu ayuda para vivir SEEK 2025.';
