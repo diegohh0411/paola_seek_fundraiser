a33689e8f3ecf520ff8fd9905f3f8354f218d1fd
Diego Hernández Herrera <diegohh0411@gmail.com>
2024-11-10 22:39:12 -0600

    Final version

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,3 +1,24 @@
 export const getID = () => {
     return 'id' + crypto.getRandomValues(new Uint32Array(2)).join('');
 }
+
+export const sum = (arr: number[]): number => {
+    return arr.reduce((prev, curr) => prev + curr, 0);
+}
+
+export const formatter = new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+
+    // These options can be used to round to whole numbers.
+    trailingZeroDisplay: 'stripIfInteger'   // This is probably what most people
+                                            // want. It will only stop printing
+                                            // the fraction when the input
+                                            // amount is a round number (int)
+                                            // already. If that's not what you
+                                            // need, have a look at the options
+                                            // below.
+    //minimumFractionDigits: 0, // This suffices for whole numbers, but will
+    // print 2500.10 as $2,500.1
+    //maximumFractionDigits: 0, // Causes 2500.99 to be printed as $2,501
+});
