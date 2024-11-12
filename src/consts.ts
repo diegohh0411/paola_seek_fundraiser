b8fa3fbd0228b51d71af7a6298c5bde822e1ad91
Diego Hernández Herrera <diegohh0411@gmail.com>
2024-11-12 13:43:23 -0600

    Reformat para hacerlo escalable, pasando toda la info customizable a un folder especial

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Diego Hernandez | SEEK 25';
-export const SITE_DESCRIPTION = 'Un universitario católico de 20 años que necesita tu ayuda para vivir SEEK 2025.';
---
+++ src/customized/consts.ts
@@ -0,0 +1,55 @@
+interface config {
+    personalName: string;
+    title: string,
+    subtitle: string,
+    moneypool?: string,
+
+    transfer: {
+        bank: string,
+        clabe: string,
+    },
+
+    expenses: {
+        transporte: number[],
+        boleto: number[],
+        hospedaje: number[],
+        comida: number[],
+    },
+
+    raised: {
+        ahorros: number[],
+        donaciones: number[],
+    },
+
+    prayerFormsUrl?: string,
+}
+
+export const globalConfig: config = {
+    personalName: 'Diego Hernández',
+    title: "Hola, soy Diego Hernández",
+    subtitle: "Un universitario católico de 20 años que necesita tu ayuda para vivir SEEK 2025.",
+    moneypool: "https://www.moneypool.mx/p/IeBnElA",
+
+    transfer: {
+        bank: "BBVA",
+        clabe: "012180015256210819",
+    },
+
+    expenses: {
+        transporte: [14000],
+        boleto: [6500],
+        hospedaje: [4500],
+        comida: [4500],
+    },
+
+    raised: {
+        ahorros: [4700],
+        donaciones: [3000],
+    },
+
+    prayerFormsUrl: "https://diego-hdz.notion.site/13be954db47680a4a3dfc1526aceb634?pvs=105",
+}
+
+export const SITE_TITLE = `${globalConfig.personalName} | SEEK 2025`;
+export const SITE_DESCRIPTION = globalConfig.subtitle;
+
